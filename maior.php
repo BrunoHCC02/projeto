76cb3b3a55f3eaad8b275ac4ad0d49546f841c21
BrunoHCC02 <brhcc02@gmail.com>
2024-08-14 22:10:00 -0300

    novo projeto

---
+++ maior.php
@@ -0,0 +1,13 @@
+<?php 
+	$dat1 = "20.5";
+	$dat2 = "10.15";
+	$dat3 = "0";
+	
+	$dat3 = $dat1+$dat2;
+
+	if ($dat3 >= $dat1) {
+		echo "Resultado é maior que $dat1.";
+	}else {
+		echo "Resultado é menor que $dat1.";
+	}
+?>
